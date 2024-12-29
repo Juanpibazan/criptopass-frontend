980d395e0807f452a60e2239521bb895235c0a92
Juanpibazan <juampivader1@gmail.com>
2024-12-29 17:40:07 -0400

    improved RegsigtroDestinatarios.jsx

--- src/sections/RegistroDestinatarios.jsx
+++ src/sections/RegistroDestinatarios.jsx
@@ -13,6 +13,7 @@ const RegistroDestinatarios = ()=>{
     const [dropdownShowing,setDropdownShowing] = useState(false);
     const [searchString,setSearchString] = useState('');
     const [searchItems, setSearchItems] = useState([]);
+    const [destinyAlias,setDestinyAlias] = useState('');
 
     const handleSearch = async (string)=>{
         try{
@@ -21,7 +22,7 @@ const RegistroDestinatarios = ()=>{
         if(string.length>=5){
         const foundCustomer = await axios({
             method:'get',
-            url:`http://localhost:4000/bridge/customers/find/${string}`,
+            url:`https://criptopass-api.onrender.com/bridge/customers/find/${string}`,
             headers:{
                 "Content-Type":"application/json",
                 "Authorization":`Bearer ${jwtoken}`
@@ -51,30 +52,68 @@ const RegistroDestinatarios = ()=>{
                 position:'top-center'
             });
     }
-    
+    };
+
+    const addDestiny = async ()=>{
+        try{
+            const notificationId = toast.loading("Por favor espere...",{
+                closeOnClick:true
+            });            
+            const response = await axios({
+                method:'post',
+                url:'https://criptopass-api.onrender.com/bridge/customers/destinatarios',
+                data:{
+                    destiny_external_account_id:searchItems[0].external_account_id,
+                    destiny_customer_id:searchItems[0].id,
+                    destiny_customer_alias: destinyAlias,
+                    origin_customer_id: user.customer_id
+                },
+                headers:{
+                    "Content-Type":"application/json",
+                    "Authorization":`Bearer ${jwtoken}`
+                }
+            });
+            const {msg} = response.data;
+            if(response.status===201){
+                toast.update(notificationId,{render:msg,type:'success',isLoading:false});
+            } else{
+                toast.update(notificationId,{render:msg,type:'error',isLoading:false});
+            }
+    } catch(e){
+            console.log(e);
+            toast(e.response.data.msg,{
+                type:'error',
+                position:'top-center'
+            });
+    }
     };
     
     return (
         <div className='px-4 py-2'>
-            <div>
+            <div className='flex justify-start items-center gap-10'>
                 <div>
-                    <h3>Buscar destinatario por email</h3>
+                    <h3 className='text-[20px] text-primary font-openSauce font-bold'>Buscar destinatario por email</h3>
                     <input type='text' value={searchString} onChange={(e)=>handleSearch(e.target.value)}
                     className={`border-secondary border-2 rounded-sm`}
                     />
                     <select>
                         {searchItems.map((item, index)=>{
                             return (
-                                <option key={index} className={`${!dropdownShowing ? 'hidden' : 'block'}`}>{(!item.first_name && !item.last_name) ? item : item.first_name + ' '+ item.last_name}</option>
+                                <option key={index} className={`${!dropdownShowing ? 'hidden' : 'block'} font-garet`}>{(!item.first_name && !item.last_name) ? item : item.first_name + ' '+ item.last_name}</option>
                             )
                         })}
                     </select>
 
                 </div>
                 <div>
-
+                    <label className='text-[20px] text-primary font-openSauce font-bold'>Asignar un alias/nickname al destinatario</label><br/>
+                    <input type='text' placeholder='Mi propia cuenta/Cuenta de mi BFF/Cuenta del proveedor 1' value={destinyAlias} onChange={(e)=>setDestinyAlias(e.target.value)} />
                 </div>
             </div>
+            <button
+            className='my-4 bg-secondary text-white font-garet px-4 py-2 border-secondary border-2 rounded-md'
+            onClick={()=>addDestiny()}
+            >Agregar destinatario</button>
             <ToastContainer position='top-center' />
         </div>
     )
