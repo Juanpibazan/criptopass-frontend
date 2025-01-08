d4e8a787972b002490790fc0b079325ee0c62db9
Juanpibazan <juampivader1@gmail.com>
2025-01-07 22:53:24 -0400

    added TOS and fixed control flow inside Profile.jsx

--- src/sections/Profile.jsx
+++ src/sections/Profile.jsx
@@ -15,8 +15,10 @@ const Profile = ()=>{
     const [email,setEmail] = useState(user ? user.email : '');
     const [fullName,setFullName] = useState(user ? `${user.first_name} ${user.last_name}` : '');
     const [type,setType] = useState(user ? user.type : '');
-    const [kycStatus,setKycStatus] = useState(user.kyc_status ? user.kyc_status : 'not started');
-    const [kycLink, setkycLink] = useState(user.kyc_link ? user.kyc_link : '');
+    const [kycStatus,setKycStatus] = useState('not started');
+    const [kycLink, setkycLink] = useState('');
+    const [tosStatus,setTosStatus] = useState('not started');
+    const [tosLink, setTosLink] = useState('');
     const [lastResponseStatus,setLastResponseStatus] = useState();
 
 
@@ -76,6 +78,10 @@ const Profile = ()=>{
     };
 
     useEffect(()=>{
+        try{
+            const notificationId = toast.loading("Por favor espere...",{
+                closeOnClick:true
+            });
         const getKYC = async ()=>{
             const kyc_link_record = await axios({
                 method:'get',
@@ -87,21 +93,59 @@ const Profile = ()=>{
             });
             setLastResponseStatus(kyc_link_record.status);
             if(kyc_link_record.status===200){
+                setKycStatus(kyc_link_record.data.kyc_status);
+                setTosStatus(kyc_link_record.data.tos_status);
                 setkycLink(kyc_link_record.data.kyc_link);
+                setTosLink(kyc_link_record.data.tos_link);
                 const {msg,data} = kyc_link_record.data;
                 if(!user.kyc_link_id || user.kyc_link_id ===''){
-                    user.kyc_link_id=data.id;
+                    /*user.kyc_link_id=data.id;
                     user.kyc_link=data.kyc_link;
-                    user.kyc_status=data.kyc_status;
-                    localStorage.setItem('user',JSON.stringify(user));
+                    user.kyc_status=data.kyc_status;*/
+                    dispatch({
+                        type: actionTypes.SET_USER,
+                        user: {
+                            ...user,
+                            kyc_link_id: data.id,
+                            kyc_link: data.kyc_link,
+                            kyc_status: data.kyc_status,
+                            tos_link:data.tos_link,
+                            tos_status:data.tos_status
+                            }
+                    });
+                    localStorage.setItem('user',JSON.stringify({
+                        ...user,
+                        kyc_link_id: data.id,
+                        kyc_link: data.kyc_link,
+                        kyc_status: data.kyc_status,
+                        tos_link:data.tos_link,
+                        tos_status:data.tos_status
+                        }));
+                    //localStorage.setItem('user',JSON.stringify(user));
                     console.log('Inside IF',user);
+                } else{
+                    setKycStatus(user.kyc_status ? user.kyc_status : data.kyc_status);
+                    setTosStatus(user.tos_status ? user.tos_status : data.tos_status);
+                    setkycLink(data.kyc_link);
+                    setTosLink(data.tos_link);
+                    //localStorage.setItem('user',JSON.stringify(user));
+                    console.log('Outside IF',user);
                 }
-                localStorage.setItem('user',JSON.stringify(user));
-                console.log('Outside IF',user);
+                toast.update(notificationId,{render:msg,type:'success',isLoading:false});
+            } else{
+                console.log(kyc_link_record);
+                toast.update(notificationId,{render:msg,type:'error',isLoading:false});
             }
         };
         getKYC();
-    },[]);
+    } catch(e){
+        console.log(e);
+        toast(e.response.data.msg,{
+            type:'error',
+            position:'top-center'
+        });
+    }
+    },[kycStatus,tosStatus]);
 
     const isTokenExpired = (token)=>{
         if(!token){
@@ -172,8 +216,31 @@ const Profile = ()=>{
                     onClick={()=>startKYC(import.meta.env.VITE_BRIDGE_API_KEY,fullName,email,type)}
                     >Comenzar proceso KYC</button>
                     ) : (
-                        <div >
-                            <p><strong>Status: </strong><span className={ `${kycStatus==='approved' ? 'bg-green-300' : 'bg-tertiary'} border-2 border-tertiary text-primary font-garet font-bold rounded-md py-2 px-4 w-[20%]`}>{kycStatus}</span></p>
+                        <div>
+                            <div >
+                                <p><strong>Status: </strong><span className={ `${kycStatus==='approved' ? 'bg-green-300' : 'bg-tertiary'} border-2 border-tertiary text-primary font-garet font-bold rounded-md py-2 px-4 w-[20%]`}>{kycStatus}</span></p>
+                            </div>
+                            <div>
+                                <p><strong> KYC Link: </strong><a href={kycLink} className={kycStatus !=='approved' ? 'text-primary bg-blue-100 font-garet font-bold text-[12px] hover:text-secondary block' : 'hidden'}>{kycLink}</a></p>  
+                            </div>
+                        </div>
+                    )}
+                </div>
+                <div className='border-primary border-3 rounded-md py-2 px-4'>
+                    <h2 className='font-openSauce font-bold text-[25px]'>TOS</h2>
+                    {kycLink ==='' ? (
+                    <button className='bg-tertiary border-2 border-tertiary
+                    text-primary font-garet font-bold rounded-md py-2 px-4'
+                    onClick={()=>startKYC(import.meta.env.VITE_BRIDGE_API_KEY,fullName,email,type)}
+                    >Aceptar TOS</button>
+                    ) : (
+                        <div>
+                            <div >
+                                <p><strong>Status: </strong><span className={ `${tosStatus==='approved' ? 'bg-green-300' : 'bg-tertiary'} border-2 border-tertiary text-primary font-garet font-bold rounded-md py-2 px-4 w-[20%]`}>{tosStatus}</span></p>
+                            </div>
+                            <div>
+                                <p><strong> TOS Link: </strong><a href={tosLink} className={tosStatus !=='approved' ? 'text-primary bg-blue-100 font-garet font-bold text-[12px] hover:text-secondary block' : 'hidden'}>{tosLink}</a></p>  
+                            </div>
                         </div>
                     )}
                 </div>
