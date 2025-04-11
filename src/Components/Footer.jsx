9b8dfb87877f6633b432d68e2c4be0a42e4fd8f1
Juanpibazan <juampivader1@gmail.com>
2025-04-11 16:40:06 -0400

    added an ip tracker but commented it out for now

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,27 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import { Link } from 'react-router-dom';
 import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+import axios from 'axios';
 
 import logoSVG from '../img/criptopass.svg';
 
 
 const Footer = ()=>{
+
+    {/*const [myIp,setMyIp] = useState('');
+    useEffect(()=>{
+        const getIpAddress = async ()=>{
+            const {data} = await axios({
+                method:'get',
+                url:'https://api.ipify.org/?format=json',
+                headers:{
+                    'Content-Type':'application/json'
+                }
+            });
+            console.log('IP Address: ', data.ip);
+    };
+    getIpAddress();
+    },[]); */}
 
     return (
         <div className='footer min-h-[300px] py-[10px] px-[15px] bg-secondary flex max-lg:flex-col lg:flex-wrap justify-evenly max-lg:justify-start items-center max-lg:items-start max-lg:gap-8'>
