1705b1129977e60c169813775439943aca025326
Juanpibazan <juampivader1@gmail.com>
2025-04-14 00:58:51 -0400

    Improved logic for showing selected destinatario from the dropdown list inside RegistroDestinatarios.jsx

--- src/sections/RegistroDestinatarios.jsx
+++ src/sections/RegistroDestinatarios.jsx
@@ -145,8 +145,10 @@ const RegistroDestinatarios = ()=>{
                     <input type='text' placeholder='Escribe la dirección de email' value={searchString} onChange={(e)=>handleSearch(e.target.value)}
                     className={`border-secondary border-2 rounded-sm w-full`}
                     />
-                    {searchItems.length > 1 ? 
-                    <select className={`${selectedItem.account_number ? 'bg-green-400' : ''}`} value={selectedItem} onChange={(e)=>setSelectedItem(JSON.parse(e.target.value))}>
+                    {searchItems.length > 1 ?
+                    <div>
+                    <input className={`${selectedItem.account_number ? 'block bg-green-400' : 'hidden'} w-full`} readOnly={true} value={(!selectedItem.first_name && !selectedItem.last_name) ? selectedItem : selectedItem.first_name + ' '+ selectedItem.last_name + ' - '+selectedItem.bank_name} />
+                    <select value={selectedItem} onChange={(e)=>setSelectedItem(JSON.parse(e.target.value))}>
                         {searchItems.map((item, index)=>{
                             return (
                                 <option key={index} value={JSON.stringify(item)}
@@ -157,7 +159,8 @@ const RegistroDestinatarios = ()=>{
                                 </option>
                             )
                         })}
-                    </select> :
+                    </select> 
+                    </div>:
                     <button readOnly={true} className={`text-left w-full ${selectedItem.account_number ? 'bg-green-400' : ''}`} value={searchItems[0]} onClick={()=>setSelectedItem(searchItems[0])}>{searchItems.length>0 ? searchItems[0].first_name+' '+searchItems[0].last_name + ' - '+ searchItems[0].bank_name : ''}</button>
                     }
                 </div>
